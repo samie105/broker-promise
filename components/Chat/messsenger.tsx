b59da767eb0a946dbd8c7008865aacbcf00fb6a9
samie105 <samsonrichfield@gmail.com>
2025-05-11 12:00:44 +0100

    update messenger script source URL

--- components/Chat/messsenger.tsx
+++ components/Chat/messsenger.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import Script from "next/script";
 
 export default function Messsenger() {
-  return <Script src="//code.tidio.co/cdxqg9u1cdyg3srs5klqcbrrzshcd7yc.js" />;
+  return <Script src="//code.jivosite.com/widget/NCblBuwhwP" />;
 }
